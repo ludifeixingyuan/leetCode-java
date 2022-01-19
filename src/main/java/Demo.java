150c4459886722ae32e5c8ed9e4a6f00c82a5811
wangzongbin <wangzongbin@wlmkids.com>
2022-01-19 17:17:11 +0800

    demo & code

---
+++ src/main/java/Demo.java
@@ -0,0 +1,34 @@
+public class Demo {
+    // 题目
+    // xxx
+    //
+    // 示例:
+    // 输入: xxx
+    // 输出: xxx
+    //
+    // 说明:
+    // xxx
+
+    // 题解：
+    // xxx
+
+    // 错题记录：
+    // xxx
+
+    public static void main(String[] args) {
+        // 标准答案
+        // answers();
+
+        // 练习
+        code();
+    }
+
+    private static void code() {
+
+    }
+
+
+    private static void answers() {
+
+    }
+}
--- src/main/java/com/pilot/leetcode/数组与矩阵/LeetCode_283_Move_Zeroes.java
+++ src/main/java/com/pilot/leetcode/数组与矩阵/LeetCode_283_Move_Zeroes.java
@@ -1,4 +1,4 @@
-package com.pilot.leetcode;
+package com.pilot.leetcode.数组与矩阵;
 
 import java.util.Arrays;
 
@@ -8,7 +8,7 @@ import java.util.Arrays;
  * @author wangzongbin
  * @date 2021-10-18
  */
-public class leetCode_283_Move_Zeroes {
+public class LeetCode_283_Move_Zeroes {
     // 题目
     // 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
     //
@@ -24,8 +24,34 @@ public class leetCode_283_Move_Zeroes {
     // 这道题让我们将一个给定数组中所有的0都移到后面，把非零数前移，要求不能改变非零数的相对应的位置关系，而且不能拷贝额外的数组，
     // 那么只能用替换法in-place来做，需要用两个指针，一个不停的向后扫，找到非零位置，然后和前面那个指针交换位置即可.
 
+    // 错题记录：
+    // 打印数组 -> Arrays.toString(array[])
+    // while (inx <= nums.length) 应该修正为 while (inx < nums.length) inx从下标0开始，length是个数
+
     public static void main(String[] args) {
         int[] nums = {0, 1, 0, 3, 12};
+        // 标准答案
+        // answers(nums);
+
+        // 练习
+        code(nums);
+    }
+
+    private static void code(int[] nums) {
+        int inx = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[inx++] = num;
+            }
+        }
+        while (inx < nums.length) {
+            nums[inx++] = 0;
+        }
+        System.out.println("输出: " + Arrays.toString(nums));
+    }
+
+
+    private static void answers(int[] nums) {
         int idx = 0;
         for (int num : nums) {
             if (num != 0) {
